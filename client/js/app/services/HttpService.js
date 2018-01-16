b60b01766dbe9a3007c66a68567c67cab42717c1
Matheus Guilherme <matheus.g.suzart@hotmail.com>
2018-01-16 17:06:17 -0200

    Video 05.02 - implementando fetch() no get()

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -14,25 +14,12 @@ class HttpService {
 
 	post(url, dado) {
 
-        return new Promise((resolve, reject) => {
+		return fetch(url, {
 
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-        });
+			headers: {'Content-type' : 'application/json'},
+			method: 'post',
+			body: JSON.stringify(dado)
+		})
+		.then(res => this._handleErrors(res));
     }
 }
